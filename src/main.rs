91146728649f496a0f4d03a98e16b82d9dfed5a3
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2024-09-10 20:12:03 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use sycamore::prelude::*;
+
+#[component]
+fn App() -> View {
+    view! {
+        h1(class="bg-red-400") { "Sycamore" }
+    }
+}
+
+fn main() {
+    console_error_panic_hook::set_once();
+
+    sycamore::render(App);
+}
