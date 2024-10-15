f5702d1ea68700a71ea2711d03024c330108038f
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2024-10-15 20:03:10 +0100

    MPA setup

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,55 @@
-use sycamore::prelude::*;
+pub mod pages;
+pub mod shell;
 
-#[component]
-fn App() -> View {
-    view! {
-        h1(class="bg-red-400") { "Sycamore!" }
+use self::shell::*;
+
+use sycamore::prelude::*;
+use sycamore_router::Route;
+
+static ROUTES: &[&str] = &["/index.html", "/404.html"];
+static PUBLIC_PATH: &str = "dist/.stage";
+
+#[cfg_ssr]
+#[tokio::main]
+async fn main() {
+    use std::path::PathBuf;
+    use tokio::fs;
+
+    for route in ROUTES {
+        let path = PathBuf::from(PUBLIC_PATH).join(route.trim_start_matches('/'));
+
+        eprintln!("Rendering `{route}` to `{}`", path.display());
+
+        let html = sycamore::render_to_string_await_suspense(|| {
+            view! {
+                Shell {
+                    sycamore_router::StaticRouter(route=Routes::default().match_path(route), view=App)
+                }
+            }
+        })
+        .await;
+
+        fs::create_dir_all(PUBLIC_PATH)
+            .await
+            .expect("failed to create public dir");
+        fs::write(path, html)
+            .await
+            .expect("failed to write html file");
     }
 }
 
+#[cfg_not_ssr]
 fn main() {
     console_error_panic_hook::set_once();
 
-    sycamore::render(App);
+    sycamore::hydrate_to(
+        || {
+            view! {
+                Shell {
+                    sycamore_router::Router(integration=sycamore_router::HistoryIntegration::new(), view=App)
+                }
+            }
+        },
+        &document(),
+    );
 }
---
+++ src/shell.rs
@@ -0,0 +1,51 @@
+use sycamore::prelude::*;
+use sycamore_router::Route;
+
+#[derive(Debug, Clone, Route)]
+pub enum Routes {
+    #[to("/")]
+    Index,
+    #[to("/index.html")]
+    IndexHtml,
+    #[to("/404.html")]
+    #[not_found]
+    NotFound,
+}
+
+#[component(inline_props)]
+pub fn Shell(children: Children) -> View {
+    let children = children.call();
+    view! {
+        html {
+            sycamore::web::NoHydrate {
+                head {
+                    script(r#type="module") {
+                        r#"import init from "/sycamore-website.js"; init();"#
+                    }
+                    link(rel="stylesheet", href="/index.css")
+                    link(rel="icon", href="/favicon.ico")
+                }
+            }
+            body {
+                (children)
+            }
+        }
+    }
+}
+
+#[component]
+pub fn App(route: ReadSignal<Routes>) -> View {
+    view! {
+        (match route.get_clone() {
+            Routes::Index | Routes::IndexHtml => view! {
+                h1 { "Index" }
+            },
+            Routes::NotFound => view! {
+                h1 { "404 Not Found" }
+                p {
+                    a(href="/") { "Return to the home page" }
+                }
+            },
+        })
+    }
+}
