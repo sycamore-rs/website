e4afd70f07e763146fc7653eb708917234e59a22
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2024-10-17 21:07:59 +0100

    Add page overview

--- src/pages/book.rs
+++ src/pages/book.rs
@@ -32,11 +32,12 @@ fn BookBody(section: String, #[prop(!optional)] doc: Option<String>) -> View {
         .clone();
 
     view! {
-        div(class="flex flex-row gap-4 w-full") {
+        div(class="flex flex-row gap-4 w-full justify-center") {
             BookSidebar {}
-            div(class="grow-0 min-w-0 mx-auto px-2 pt-0 pb-10 sm:pt-5 prose prose-gray") {
+            div(class="grow-0 min-w-0 px-2 pt-0 pb-10 sm:pt-5 prose prose-gray") {
                 mdsycx::MDSycX(body=parsed.body)
             }
+            PageOverview {}
         }
     }
 }
@@ -63,7 +64,7 @@ fn BookSidebar() -> View {
                 .collect::<Vec<_>>();
             view! {
                 div(class="pl-2") {
-                    a(class="font-semibold") { (section.title.clone()) }
+                    a(class="font-bold") { (section.title.clone()) }
                     ul(class="pl-2") {
                         (items)
                     }
@@ -74,10 +75,40 @@ fn BookSidebar() -> View {
 
     view! {
         div(
-            class="flex-none w-44 pt-8 pb-5 pr-2 text-sm sticky top-12 max-h-[calc(100vh-3rem)] overflow-y-auto
-                hidden sm:block"
+            class="flex-none w-44 pt-8 pb-5 pr-2 text-sm sticky top-12 max-h-[calc(100vh-3rem)] overflow-y-auto hidden sm:block"
         ) {
             (view)
+        }
+    }
+}
+
+#[cfg_ssr]
+#[component]
+fn PageOverview() -> View {
+    view! {
+        div(
+            class="flex-none w-44 pt-8 pb-5 pr-2 text-sm sticky top-12 max-h-[calc(100vh-3rem)] overflow-y-auto hidden lg:block"
+        ) {
+            div {
+                p(class="font-bold") { "On this page" }
+            }
+            div {
+                p(class="font-bold") { "Previous versions" }
+                ul(class="pl-2") {
+                    li {
+                        a(href="https://sycamore-rs.netlify.app/docs/v0.8/getting_started/installation") {
+                            "v0.8"
+                            i(class="bi bi-box-arrow-up-right ml-2")
+                        }
+                    }
+                    li {
+                        a(href="https://sycamore-rs.netlify.app/docs/v0.7/getting_started/installation") {
+                            "v0.7"
+                            i(class="bi bi-box-arrow-up-right ml-2")
+                        }
+                    }
+                }
+            }
         }
     }
 }
