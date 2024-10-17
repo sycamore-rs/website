6673e9c55244096a83456ef7af8299a626e47b92
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2024-10-17 21:57:54 +0100

    Add headings outline

--- src/pages/book.rs
+++ src/pages/book.rs
@@ -37,7 +37,7 @@ fn BookBody(section: String, #[prop(!optional)] doc: Option<String>) -> View {
             div(class="grow-0 min-w-0 px-2 pt-0 pb-10 sm:pt-5 prose prose-gray") {
                 mdsycx::MDSycX(body=parsed.body)
             }
-            PageOverview {}
+            HeadingsOutline(headings=parsed.headings)
         }
     }
 }
@@ -83,14 +83,37 @@ fn BookSidebar() -> View {
 }
 
 #[cfg_ssr]
-#[component]
-fn PageOverview() -> View {
+#[component(inline_props)]
+fn HeadingsOutline(headings: Vec<mdsycx::OutlineHeading>) -> View {
+    let outline = headings
+        .into_iter()
+        .map(|heading| {
+            let href = format!("#{}", heading.id);
+            let class = "hover:text-orange-700 transition-colors";
+            match heading.level {
+                1 => view! {},
+                2 => view! {
+                    li(class="mt-2") {
+                        a(href=href, class=format!("{class} font-semibold")) { (heading.text) }
+                    }
+                },
+                _n => view! {
+                    li(class="mt-0.5 ml-2") {
+                        a(href=href, class=format!("{class} pl-2")) { (heading.text) }
+                    }
+                },
+            }
+        })
+        .collect::<Vec<_>>();
     view! {
         div(
-            class="flex-none w-44 pt-8 pb-5 pr-2 text-sm sticky top-12 max-h-[calc(100vh-3rem)] overflow-y-auto hidden lg:block"
+            class="flex-none w-56 pt-8 pb-5 pr-2 text-sm sticky top-12 max-h-[calc(100vh-3rem)] overflow-y-auto hidden lg:block"
         ) {
             div {
                 p(class="font-bold") { "On this page" }
+                ul(class="pl-2") {
+                    (outline)
+                }
             }
             div {
                 p(class="font-bold") { "Previous versions" }
